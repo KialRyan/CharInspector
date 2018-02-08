1ff44fa32311c9e2bc19836a4c1f310a03667924
KialRyan <kleiteryan@mail.csuchico.edu>
2018-02-07 18:58:38 -0800

    Asks for a character and responds with the value

--- main.cpp
+++ main.cpp
@@ -7,26 +7,25 @@ int main()
 {
   char letter; 
   cout<<"What character do you want to know about?\n";
-  cin>> letter
-  if (letter>='A'&&letter<='Z')
+  cin>> letter;
+  if (letter>='A'&& letter<='Z')
   {
   //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
+  cout<< letter << " is an upper case letter!\n";
   }
-  if (letter>='a'&& letter<'z'=)
+  if (letter>='a'&& letter<='z')
   {
   //when user's entry is between a-z...
-  cout<<" is a lower case letter!\n";
+  cout<< letter <<  " is a lower case letter!\n";
   }
-  else
+  if (letter<='!')
   {
   //in all other cases...
   cout<<"?! Pssh. What are you talking about?\n";
   }
   //no matter what they enter...
-  cout<<"The ASCII value is: " 
-  (int)letter;
-  cout<<(int)letter;
+  cout<<"The ASCII value is: " << (int) letter <<endl; 
+ 
 
   return 0;
 }
