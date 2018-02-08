b8a7056d1d3b012bd80214c9b12d194cfc5d9768
KialRyan <kleiteryan@mail.csuchico.edu>
2018-02-07 19:55:40 -0800

    Fixes the error with || missing in the third if

--- main.cpp
+++ main.cpp
@@ -8,21 +8,25 @@ int main()
   char letter; 
   cout<<"What character do you want to know about?\n";
   cin>> letter;
+
   if (letter>='A'&& letter<='Z')
   {
   //when user's entry is between A-Z...
   cout<< letter << " is an upper case letter!\n";
   }
+
   if (letter>='a'&& letter<='z')
   {
   //when user's entry is between a-z...
   cout<< letter <<  " is a lower case letter!\n";
   }
-  if (letter<='!')
+
+  if (letter<='A'|| letter>='z')
   {
   //in all other cases...
   cout<<"?! Pssh. What are you talking about?\n";
   }
+
   //no matter what they enter...
   cout<<"The ASCII value is: " << (int) letter <<endl; 
  
