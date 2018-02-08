56ef79eea2aaa002a4597f30b97ff526ae2b582c
KialRyan <kleiteryan@mail.csuchico.edu>
2018-02-07 18:25:49 -0800

    Adds the If statements needed

--- main.cpp
+++ main.cpp
@@ -5,20 +5,28 @@ using namespace std;
 
 int main()
 {
-
+  char letter; 
   cout<<"What character do you want to know about?\n";
-
+  cin>> letter
+  if (letter>='A'&&letter<='Z')
+  {
   //when user's entry is between A-Z...
   cout<<" is an upper case letter!\n";
-
+  }
+  if (letter>='a'&& letter<'z'=)
+  {
   //when user's entry is between a-z...
   cout<<" is a lower case letter!\n";
-
+  }
+  else
+  {
   //in all other cases...
   cout<<"?! Pssh. What are you talking about?\n";
-  
+  }
   //no matter what they enter...
-  cout<<"The ASCII value is: ";
+  cout<<"The ASCII value is: " 
+  (int)letter;
+  cout<<(int)letter;
 
   return 0;
 }
